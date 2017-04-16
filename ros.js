b2a4ad8f7e7573cb35cbcfb721cd9cf31e774103
prajankya <prajankya@gmail.com>
2017-04-16 14:49:27 +0530

    added publisher

--- ros.js
+++ ros.js
@@ -3,15 +3,19 @@
 const path = require('path');
 
 module.exports = function(RED) {
-  var RosNodeConfig = require(path.join(
-    __dirname,
-    'ros-node-config',
-    'ros-node-config'
-  ))(RED)
+	var RosNodeConfig = require(path.join(
+		__dirname,
+		'ros-node-config',
+		'ros-node-config'
+	))(RED)
 
-  RED.nodes.registerType("ros-node-config", RosNodeConfig)
+	RED.nodes.registerType("ros-node-config", RosNodeConfig)
 
 
-  var RosSubscriberNode = require(path.join(__dirname, 'ros-subscriber', 'ros-subscriber'))(RED)
-  RED.nodes.registerType("ros-subscriber", RosSubscriberNode)
+	var RosSubscriberNode = require(path.join(__dirname, 'ros-subscriber', 'ros-subscriber'))(RED)
+	RED.nodes.registerType("ros-subscriber", RosSubscriberNode)
+
+
+	var RosPublisherNode = require(path.join(__dirname, 'ros-publisher', 'ros-publisher'))(RED)
+	RED.nodes.registerType("ros-publisher", RosPublisherNode)
 }
